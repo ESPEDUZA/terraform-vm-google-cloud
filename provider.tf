952604b9d72ce1d41a7a23f4f18dd0e0d1d3a270
ESPEDUZA <75570746+ESPEDUZA@users.noreply.github.com>
2023-12-23 17:18:42 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
   credentials = file("your-credenttials-file.json")
-  project     = "endless-gamma-408916"
+  project     = "your-project-id"
   region      = "eu-central1" // e.g., "us-central1"
   zone        = "europe-west2-c"   // e.g., "us-central1-a"
 }
